d990317d4ff5b2b1bf0bdb5d7ff6f4e4f972829d
shahjavadeveloper <shahjavadeveloper@gmail.com>
2023-07-06 15:11:33 +0530

    second  commit

--- src/test/java/com/shah/jenkinsapi/SpringJenkinsApplicationTests.java
+++ src/test/java/com/shah/jenkinsapi/SpringJenkinsApplicationTests.java
@@ -15,6 +15,7 @@ class SpringJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		log.info("test case executing.........");
+		log.info("test case executing second log statement.........");
 		assertEquals(true, true);
 	}
 
